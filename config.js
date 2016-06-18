9cf71ddacf702d739d91f70a61b0ec31118b51d5
Roan <rohankoid@gmail.com>
2016-06-18 19:06:17 +0200

    configuration changed as module

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
-var config = {
-    apiKey: "AIzaSyDOwtOGYMB3L10WfvRUlZjodTwYSoRZUSY",
-    authDomain: "wherecanwedance.firebaseapp.com",
-    databaseURL: "https://wherecanwedance.firebaseio.com",
-    storageBucket: "wherecanwedance.appspot.com",
-    serviceAccount: "wherecanwedance-71044c4af5f0.json",
-};
+module.exports = {
+    config: function () {
+        var configuration = {
+            apiKey: "AIzaSyDOwtOGYMB3L10WfvRUlZjodTwYSoRZUSY",
+            authDomain: "wherecanwedance.firebaseapp.com",
+            databaseURL: "https://wherecanwedance.firebaseio.com",
+            storageBucket: "wherecanwedance.appspot.com",
+            serviceAccount: "wherecanwedance-71044c4af5f0.json",
+        };
+        return configuration;
+    }
+}
