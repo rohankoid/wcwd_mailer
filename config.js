0d060ed0bc87ce1f3881f7c6715962b9f52eb97b
Roan <rohankoid@gmail.com>
2016-06-18 18:19:29 +0200

    config.js added

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyDOwtOGYMB3L10WfvRUlZjodTwYSoRZUSY",
+    authDomain: "wherecanwedance.firebaseapp.com",
+    databaseURL: "https://wherecanwedance.firebaseio.com",
+    storageBucket: "wherecanwedance.appspot.com",
+    serviceAccount: "wherecanwedance-71044c4af5f0.json",
+};
