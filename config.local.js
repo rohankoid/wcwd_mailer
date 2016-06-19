490007e79878d05da5cf1d3997b868a069831f9d
Roan <rohankoid@gmail.com>
2016-06-19 17:10:07 +0200

    moment added email template created bug with user attribute update fixed. cron set to 17.15 Berlin time.

---
+++ config.local.js
@@ -0,0 +1,12 @@
+module.exports = {
+    config: function () {
+        var configuration = {
+            apiKey: "AIzaSyBVVVWTFTTh8XFgzP13U2jRbwmEJZRqHy4",
+            authDomain: "scorching-torch-8556.firebaseapp.com",
+            databaseURL: "https://scorching-torch-8556.firebaseio.com",
+            storageBucket: "scorching-torch-8556.appspot.com",
+            serviceAccount: "wcwd-4e7eef02abc3.json",
+        };
+        return configuration;
+    }
+}
--- server.js
+++ server.js
@@ -5,43 +5,116 @@ var express = require('express'),
     nodemailer = require("nodemailer"),
     CronJob = require('cron').CronJob,
     firebase = require('firebase'),
-    config = require('./config');
+    config = require('./config'),
+    moment = require('moment');
 
-app.set('port', (process.env.PORT || 5000));app.get('/', function(request, response) {
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function (request, response) {
     var result = 'App is running'
     response.send(result);
-}).listen(function() {
+}).listen(function () {
     console.log('App is running');
     //the rest of our app lives here, wrapped inside this function
-     });
+});
 
 firebase.initializeApp(config.config());
+var now = moment.now();
+var seven_days = moment().add(7, "days");
+console.log(moment().subtract(7, "days").valueOf());
+var unix_seven_days = seven_days.valueOf();
 var db = firebase.database();
-var ref = db.ref("/users");
-ref.once("value", function (snapshot) {
-    var users = snapshot.val();
-    console.log(users);
-})
+var user_ref = db.ref("/users");
+var event_ref = db.ref("/events");
 
 
-new CronJob('44 16 * * *', function () {
-    giphy.search({
-        q: 'giraffes',
-        limit: 100,
-        rating: 'g'
-    }, function (err, res) {
-        var gifs = res.data;
-        var gif = gifs[Math.floor(Math.random() * gifs.length)];
-        smtpTransport.sendMail({
-            from: process.env.MY_EMAIL, // sender address
-            to: "rohanmaleku@gmail.com", // receiver address
-            subject: "DAILY GIF ✔", // subject
-            text: gif.images.downsized_large.url // body
-        }, function (error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Message sent: " + response.message);
+new CronJob('15 17 * * *', function () {
+    console.log('Job Started');
+    var filtered_events = [];
+// get list of events which starts this week
+// (7 days is the maximum time for which notifications are sent)
+    event_ref.orderByChild("start_date").startAt(now).endAt(unix_seven_days).once("value", function (events_snapshot) {
+        var events = events_snapshot.val();
+        console.log('Events Fetched');
+        user_ref.once("value", function (users_snapshot) {
+            var users = users_snapshot.val();
+            for (var u in users) {
+                var notify = false;
+                var user_events = [];
+                var user = users[u];
+                var preferences = user.preferences ? user.preferences : {};
+                if (Object.keys(preferences).length > 0 && preferences.notify) {
+                    var notify_in = parseInt(preferences.notify);
+                    var last_notified_on = new Date(user.notified_on);
+                    var next_notify_on = moment(last_notified_on).add(notify_in, "days").valueOf();
+                    if (!isNaN(next_notify_on) && (now > next_notify_on)) // check if user needs to be notified or not
+                    {
+                        notify = true;
+                        Object.prototype.getKeyByValue = function (value) {
+                            var result = [];
+                            for (var prop in this) {
+                                if (this.hasOwnProperty(prop)) {
+                                    if (this[prop] === value)
+                                        result.push(prop);
+                                }
+                            }
+                            return result;
+                        }
+                        var event_types = preferences.event_types;
+                        var week_days = preferences.week_days;
+                        var check_days = week_days.getKeyByValue(true);
+                        var check_types = event_types.getKeyByValue(true);
+                        for (var e in events) {
+                            var event = events[e];
+                            if (event.type != undefined) {
+                                var in_type = check_types.indexOf(event.type);
+                                var in_day = check_days.indexOf(event.week_day);
+                                if ((in_day > -1) && (in_type > -1)) {
+                                    // this user should get an email
+                                    user_events.push(event);
+                                }
+                            }
+                        }
+                    }
+                }
+                if (notify) {
+                    filtered_events.push({user_id: u, user: user, events: user_events, next_notify_on: next_notify_on});
+                }
+            }
+        }).then(function () {
+            for (var e in filtered_events) {
+                var user_details = filtered_events[e];
+                var user_id = user_details.user_id;
+                var email = user_details.user.email;
+                var text = "";
+                var id = 1;
+                // build email
+                for (var ue in user_details.events) {
+                    var event = user_details.events[ue];
+                    if (event.type != undefined) {
+                        if (id == 1) {
+                            text += "Greetings " + user_details.user.name + ", <br><br>";
+                            text += "Here are some of the events happening this week, that you might be interested in. <br><br><br>";
+                        }
+                        text += id + ". <strong>" + event.venue + "</strong>, " + "<div>" + event.type + " - " + event.style + "</div></br></br>";
+                        id++;
+                    }
+
+                }
+                //update user has been notified
+                var u_ref = user_ref.child(user_id);
+                u_ref.update({notified_on: user_details.next_notify_on});
+                smtpTransport.sendMail({
+                    from: process.env.MY_EMAIL, // sender address
+                    to: email, // receiver address
+                    subject: "Where can we dance this week??", // subject
+                    html: text // body
+                }, function (error, response) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log("Message sent: " + response.message);
+                    }
+                });
             }
         });
     });
